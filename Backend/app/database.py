b1ee23e1cf827eb26ef860a23731178a8371b2a4
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2026-01-21 18:26:02 +0530

    Created sqlite.db file.

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,21 +1,8 @@
-import json
-
-shipments = {}
-
-### Load from .json file
-with open("shipments.json") as json_file:
-    data = json.load(json_file)
-
-    # Map as dictionary
-    for value in data:
-        shipments[value["id"]] = value
+import sqlite3
 
 
-### Save changes to .json file
-def save():
-    with open("shipments.json", "w") as json_file:
-        json.dump(
-            # Convert to list of shipments
-            list(shipments.values()),
-            json_file,
-        )
+### Make the connection
+connection = sqlite3.connect("sqlite.db")
+
+### Close the connection when done
+connection.close()
